b405f13f39522e30d782f762439ec4bd64bb7a0e
ntirushwajeanmarc <ntirushwajeanmarc@gmail.com>
2025-10-08 10:56:19 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>The agenda will be here soon </h1>
+    </>
+  )
+}
+
+export default App
